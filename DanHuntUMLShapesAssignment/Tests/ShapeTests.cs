ee6536f4d1e5f06f5488aade99cd43b7d1584dfd
Dan Hunt <daniel.hunt@sumtotalsystems.com>
2020-02-07 10:28:44 -0600

    A few more constructor tests

--- DanHuntUMLShapesAssignment/Tests/ShapeTests.cs
+++ DanHuntUMLShapesAssignment/Tests/ShapeTests.cs
@@ -1,5 +1,6 @@
 ﻿
 using System;
+using DanHuntUMLShapesAssignment.Shapes;
 using NUnit.Framework;
 
 namespace DanHuntUMLShapesAssignment.Tests
@@ -9,6 +10,7 @@ namespace DanHuntUMLShapesAssignment.Tests
         public class ConstructorTests
         {
             private double _defaultSize;
+            private AbstractShape sut;
 
             [SetUp]
             public void SetUp()
@@ -16,10 +18,11 @@ namespace DanHuntUMLShapesAssignment.Tests
                 _defaultSize = AbstractShape.DefaultSize;
             }
 
+            #region Square
             [Test]
             public void SquareDefaultConstructor()
             {
-                var sut = new Square();
+                sut = new Square();
 
                 Assert.That(sut.CalculatePerimeter(), Is.EqualTo(_defaultSize * 4));
                 Assert.That(sut.CalculateArea(), Is.EqualTo(Math.Pow(_defaultSize,2)));
@@ -29,7 +32,7 @@ namespace DanHuntUMLShapesAssignment.Tests
             public void SquareParameterizedConstructor()
             {
                 var len = 10;
-                var sut = new Square(len);
+                sut = new Square(len);
 
                 Assert.That(sut.CalculatePerimeter(), Is.EqualTo(len * 4));
                 Assert.That(sut.CalculateArea(), Is.EqualTo(Math.Pow(len, 2)));
@@ -41,11 +44,46 @@ namespace DanHuntUMLShapesAssignment.Tests
                 var len = 15;
                 var squareToCopy = new Square(len);
 
-                var sut = new Square(squareToCopy);
-
-                Assert.That(sut.CalculatePerimeter(), Is.EqualTo(len * 4));
-                Assert.That(sut.CalculateArea(), Is.EqualTo(Math.Pow(len, 2)));
+                sut = new Square(squareToCopy);
+                
+                Assert.That(sut.CalculateArea(), Is.EqualTo(squareToCopy.CalculateArea()));
+                Assert.That(sut.CalculatePerimeter(), Is.EqualTo(squareToCopy.CalculatePerimeter()));
             }
+            #endregion
+            #region Rectangle
+            [Test]
+            public void RectangleDefaultConstructor()
+            {
+                var sut = new Rectangle();
+
+                Assert.That(sut.CalculatePerimeter(), Is.EqualTo(_defaultSize * 2 + _defaultSize * 4));
+                Assert.That(sut.CalculateArea(), Is.EqualTo(_defaultSize * (_defaultSize * 2)));
+            }
+
+            [Test]
+            public void RectangleParameterizedConstructor()
+            {
+                var width = 10;
+                var height = 20;
+                sut = new Rectangle(width, height);
+
+                Assert.That(sut.CalculatePerimeter(), Is.EqualTo(2 * width + 2 * height));
+                Assert.That(sut.CalculateArea(), Is.EqualTo(width * height));
+            }
+
+            [Test]
+            public void RectangleCopyConstructor()
+            {
+                var width = 5;
+                var height = 15;
+                var rectangleToCopy = new Rectangle(width, height);
+
+                sut = new Rectangle(rectangleToCopy);
+
+                Assert.That(sut.CalculateArea(), Is.EqualTo(rectangleToCopy.CalculateArea()));
+                Assert.That(sut.CalculatePerimeter(), Is.EqualTo(rectangleToCopy.CalculatePerimeter()));
+            }
+            #endregion
         }
     }
 }
